49de9eef1e60804821e16d1a261071b063f2886c
Dev_al <alorcilla11@icloud.com>
2026-03-25 07:14:46 +0800

    Switch to permanent PostgreSQL database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -53,12 +53,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-# Database
+import os
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 # Internationalization
